9382570213e6b1f4a71f49154ab4a0f23bf6fc1b
Kabuñiang Buhawi Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-06-04 17:30:33 +0200

    Update get_next_line.c
    updated skeleton to new logic

--- get_next_line.c
+++ get_next_line.c
@@ -12,58 +12,110 @@
 
 #include "get_next_line.h"
 
-// Function that reads a line from file descriptor
-char	*get_next_line(int fd)
+static char	*ft_left_over(char	*buffer)
 {
-	static char	**buffer_array;
-	char	*next_line;
+	char	*out;
+	size_t	i;
+	size_t	j;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
-		return (NULL);
-	buffer_array = malloc();
-	fill_array(buffer_array, fd);
-}
-
-// Fill buffer array with buffers
-int	fill_array(char **buffer_array, int fd)
-{
-	size_t	file_chars;
-	char	*buffer;
-	int		concat_flag;
-
-	while (!ft_strchr(buffer_array))
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
 	{
-		buffer = malloc(BUFFER_SIZE + 1);
-		if (!buffer)
-			return ;
-		file_chars = read(fd, buffer, BUFFER_SIZE);
-		buffer[file_chars] = '\0';
-		concat_flag = concat_buffer(buffer_array, buffer);
+		free(buffer);
+		return (NULL);
 	}
+	out = malloc(sizeof(char) * (ft_strlen(buffer) - i + 1));
+	i++;
+	while (buffer[i])
+	{
+		out[j] = buffer[i];
+		i++;
+		j++;
+	}
+	free(buffer);
+	return (out);
 }
 
-// Add buffer to buffer array
-int	concat_buffer(char **buffer_array, char *buffer)
+static char	*ft_line_maker(char *buffer)
 {
+	char	*line;
 	size_t	i;
 
 	i = 0;
-	while (buffer_array[i] != NULL)
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] != '\0' && buffer[i] != '\n')
 		i++;
-	buffer_array[i] = buffer;
-	buffer_array[++i] = NULL;
+	//Check for new line or \0
+	line = malloc(sizeof(char) * (i + 2));
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	// May return error
+	if (buffer[i] == '\0' || buffer[i])
+		line[i++] ='\n';
+	return (line);
 }
 
-//Find char c in string s: return first found address, NULL otherwise
-char	*ft_bufferchr(const char **s, int c)
+// Helper for main logic to read line into buffer and retain leftovers
+static ssize_t	ft_read_file(int fd, char *buffer, char *left_c)
 {
-	while (*s)
+	char	*temp;
+	ssize_t	out_bytes;
+
+	out_bytes = 1;
+	while (out_bytes > 0)
 	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
+		out_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (out_bytes == -1)
+			return (out_bytes);
+		buffer[out_bytes] = '\0';
+		temp = ft_strjoin(left_c, buffer);
+		left_c = temp;
+		free(temp);
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
-	if ((char)c == '\0')
-		return ((char *)s);
-	return (NULL);
+	free(buffer);
+	return (left_c);
 }
+
+// Helper function to help prepare buffer & leftovers to be checked
+static char	*ft_line_prep(int fd, char *left_c)
+{
+	char	*buffer;
+	char	*out;
+
+	if (!left_c)
+	{
+		left_c = malloc(sizeof(char) * 1);
+		left_c[0] = '\0';
+	}
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	out = ft_read_file(fd, buffer, left_c);
+	return (out);
+}
+
+// Function that reads a line from file descriptor
+char	*get_next_line(int fd)
+{
+	static char	*left_c;
+	char	*buffer;
+	char	*new_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buffer, 0) < 0)
+		return (NULL);
+	buffer = ft_line_prep(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	new_line = ft_line_maker(buffer);
+	buffer = ft_left_over(buffer);
+	return (new_line);
+}
+
