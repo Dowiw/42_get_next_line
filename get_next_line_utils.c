1bd4eab32777833e1575e41811e4cd9a7270ae80
Kabuñiang Buhawi Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-06-06 21:19:47 +0200

    Update get_next_line_utils.c
    removing function dependency and allowing space for base functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,17 +12,6 @@
 
 #include "get_next_line.h"
 
-//Returns length of string (char *)
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 //Find char c in string s: return first found address, NULL otherwise
 char	*ft_strchr(const char *s, int c)
 {
@@ -43,16 +32,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*out;
 	size_t	i;
 	size_t	j;
-	size_t	total_len;
 
 	i = 0;
 	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	total_len = ft_strlen(s1) + ft_strlen(s2);
-	out = malloc(sizeof(char) * (total_len + 1));
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	out = malloc(sizeof(char) * (i + j + 1));
 	if (!out)
 		return (NULL);
+	i = 0;
+	j = 0;
 	while (s1[i])
 		out[j++] = s1[i++];
 	i = 0;
