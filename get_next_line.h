abff7a1a63fe7261214192c28aaf3a84ebfd826f
Kabuñiang Buhawi Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-06-06 22:25:22 +0200

    Update get_next_line.h
    added helpers

--- get_next_line.h
+++ get_next_line.h
@@ -22,7 +22,10 @@
 
 char	*get_next_line(int fd);
 
+char	*ft_left_over(char	*buffer);
+char	*ft_line_maker(char *buffer);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_read_file(int fd, char *buffer, char *left_c);
 
 #endif
